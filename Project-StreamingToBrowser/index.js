a28d2ef7322a5435f2a6f0715fe82a53d9b0109a
Charlie <phunghiep2709@gmail.com>
2019-02-02 10:40:26 +0700

    stream video to browser

--- Project-StreamingToBrowser/index.js
+++ Project-StreamingToBrowser/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
-const {createReadStream} = require("fs")
-const App = express();
+const { createReadStream } = require("fs");
+const video = "./ZED HIGHLIGHT.mp4";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "video/mp4" });
+  createReadStream(video).pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log("Server on PORT 3000");
+});
